afcfd510a3c9c96da9a78c93ba5a237ebf698c9b
“codewitherrors” <codewitherrors1@gmail.com>
2023-02-26 15:10:38 +0530

    Add : Frequency file

---
+++ Findfrequency.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableDemo
+{
+    
+    public class Findfrequency
+    {
+
+        public MyMapNode head;   
+        public void addlist(string data)
+        {
+            MyMapNode newnode = new MyMapNode(data);
+            
+            if(this.head == null)
+            {
+               this.head = newnode;
+            }
+            else
+            {
+                MyMapNode temp = head;
+                while(temp.key != null)
+                {
+                    temp = temp.key;
+                }
+                temp.key = newnode;
+            }
+            Console.WriteLine("{0}", newnode.value);
+            
+        }
+
+   
+        
+    }
+}
