e88715c4b2fe3b7027aa9b81252072918af01177
“codewitherrors” <codewitherrors1@gmail.com>
2023-02-26 19:43:57 +0530

    Add : Find Frequency file

--- Findfrequency.cs
+++ Findfrequency.cs
@@ -8,33 +8,69 @@ using System.Threading.Tasks;
 
 namespace HashTableDemo
 {
-    
-    public class Findfrequency
+    public class MyMapNode<k, v>
     {
+        private readonly int size;
+        private readonly LinkedList<KeyValuePair<k, v>>[] items;
 
-        public MyMapNode head;   
-        public void addlist(string data)
+
+        public struct KeyValuePair<k, v>
         {
-            MyMapNode newnode = new MyMapNode(data);
-            
-            if(this.head == null)
-            {
-               this.head = newnode;
-            }
-            else
-            {
-                MyMapNode temp = head;
-                while(temp.key != null)
-                {
-                    temp = temp.key;
-                }
-                temp.key = newnode;
-            }
-            Console.WriteLine("{0}", newnode.value);
-            
+            public k key { get; set; }
+            public v value { get; set; }
+        }
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValuePair<k, v>>[size];
         }
 
-   
-        
+        protected int GetArrayPosition(k key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+
+        public v Get(k key) 
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValuePair<k, v>> linkedList = GetLinkedList(position);
+            foreach(KeyValuePair<k,v> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(v);
+
+        }
+        public void Add(k key, v value) 
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValuePair<k, v>> linkedList = GetLinkedList(position);
+            KeyValuePair<k, v> item = new KeyValuePair<k, v>() { Key = key, Value = value };
+            linkedList.AddLast(item);
+        }
+
+        public void Remove(k key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValuePair<k, v>> linkedList = GetLinkedList(position);
+            bool itemFound = false;
+            KeyValuePair<k,v> foundItem = default(KeyValuePair<k,v>);
+            foreach(KeyValuePair<k,v> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+            }
+            if (itemFound)
+            {
+                linkedList.Remove(foundItem);
+            }
+        }
     }
 }
