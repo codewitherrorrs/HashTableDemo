7ebb20d70c569c583b61114fd867bf8f3fcc37ae
“codewitherrors” <codewitherrors1@gmail.com>
2023-02-26 15:11:07 +0530

    Add : Node.cs File

---
+++ Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableDemo
+{
+    public class MyMapNode
+    {
+        public string value;
+        public MyMapNode key;
+        public  MyMapNode(string value)
+        {
+            this.value = value;
+        }
+    }
+}
